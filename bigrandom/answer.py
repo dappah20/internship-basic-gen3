a8133399455ef32e73a5a93ba1fc6a121352ec45
dappah20 <irfandio.daff@gmail.com>
2017-02-24 14:13:04 +0700

    Update answer.py

--- bigrandom/answer.py
+++ bigrandom/answer.py
@@ -4,13 +4,14 @@ class BigRandom:
         # add attributes if you need more
 
     def answer(self):
-        noh = 0 # variable to store number of hashtag
-                # ommiting line number's hashtag
-        suc = 0 # variable to store sum of character's code in ascii,
-                # ommiting line number and its hashtag
+        noh, suc = 0, 0
+        nom = ''.join([str(x) for x in range(10000)])
+        for line in self.data.readline():
+            noh = 0
+            suc = 0 
 
         # your algorithm
 
-        return (noh,suc)
+        return (noh - 10000 ,suc - (ord('#') * 10000 + sum([ord(x) for x in nom])))
 
     # add methods if you need more
